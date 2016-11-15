ba32c79cae9c945e7c5e144826df60f9e058bee9
Nathan Rajlich <nathan@tootallnate.net>
2016-11-15 14:32:35 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * Silly little `debug()` extension that does performance measuring,
+ * sort of like `console.time()`/`console.timeEnd()` with a `%t` formatter.
+ *
+ * ```js
+ * const time = require('debug-time')
+ * const debug = require('debug')('foo')
+ *
+ * const startTime = time()
+ * setTimeout(() => {
+ *   debug('took %t milliseconds', startTime)
+ * }, 1000)
+ * ```
+ */
+const DEBUG = require('debug')
+
+DEBUG.formatters.t = (v) => {
+  const diff = process.hrtime(v)
+  return Math.round((diff[0] * 1e9 + diff[1]) * 1e-6)
+}
+
+module.exports = function time () {
+  return process.hrtime()
+}
